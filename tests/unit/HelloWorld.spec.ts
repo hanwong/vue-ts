50233463336a700038e462342404be9cb463b6dc
hanwong <hanationbear@gmail.com>
2018-08-30 11:06:21 +0900

    testing

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -8,10 +8,12 @@ describe('HelloWorld.vue', () => {
     cmp = mount(HelloWorld, { // Create a shallow instance of the component
       propsData: { msg: 'new message' }
     })
-  })
+  });
+
   it('renders props.msg when passed', () => {
     expect(cmp.vm.msg).toEqual('new message');
   });
+
   it('has the expected html structure', () => {
     expect(cmp.element).toMatchSnapshot();
   });
