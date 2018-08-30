20d57865e3b239858bf9d79cacf5c226bd5a99ab
Han Woong Kim <hanwoongkim@Hanui-MacBook-Pro.local>
2018-08-30 11:01:26 +0900

    jest testing

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,12 +1,18 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld/index.vue';
 
 describe('HelloWorld.vue', () => {
+  let cmp: any
+
+  beforeEach(() => {
+    cmp = mount(HelloWorld, { // Create a shallow instance of the component
+      propsData: { msg: 'new message' }
+    })
+  })
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    expect(cmp.vm.msg).toEqual('new message');
+  });
+  it('has the expected html structure', () => {
+    expect(cmp.element).toMatchSnapshot();
   });
 });
