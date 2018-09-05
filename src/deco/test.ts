417a16ff6e8fde2b811ade1b4c538755b0b949ee
hanwong <hanationbear@gmail.com>
2018-09-05 18:37:13 +0900

    deco test ...

--- src/deco/test.ts
+++ src/deco/test.ts
@@ -1,7 +1,8 @@
 import { createDecorator } from 'vue-class-component'
+import { ComponentOptions, ComputedOptions } from 'vue';
 
-export const NoCache = createDecorator((options, key) => {
+export const NoCache = createDecorator((options: ComponentOptions<any>, key) => {
   // component options should be passed to the callback
   // and update for the options object affect the component
-  options.computed[key].cache = false
+  (options.computed[key] as ComputedOptions<any>).cache = false
 })
