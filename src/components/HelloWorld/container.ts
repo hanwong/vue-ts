058f803c69249c399c23a0dcc10e1371ad48af11
hanwong <hanationbear@gmail.com>
2018-09-05 17:39:40 +0900

    decorator test

--- src/components/HelloWorld/container.ts
+++ src/components/HelloWorld/container.ts
@@ -1,6 +1,13 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { NoCache } from '@/deco/test';
 
 @Component
 export default class Container extends Vue {
   @Prop() private msg!: string;
+
+  @NoCache
+  get computedMsg () {
+    return this.msg;
+  }
+
 }
