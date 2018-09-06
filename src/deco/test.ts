af8f7eb5737d8c8f4e097d35729173862fe819cf
hanwong <hanationbear@gmail.com>
2018-09-06 14:32:07 +0900

    add router, vuex

--- src/deco/test.ts
+++ src/deco/test.ts
@@ -2,7 +2,5 @@ import { createDecorator } from 'vue-class-component'
 import { ComponentOptions, ComputedOptions } from 'vue';
 
 export const NoCache = createDecorator((options: ComponentOptions<any>, key) => {
-  // component options should be passed to the callback
-  // and update for the options object affect the component
-  (options.computed[key] as ComputedOptions<any>).cache = false
+  (options.computed[key] as ComputedOptions<any>).cache = false;
 })
